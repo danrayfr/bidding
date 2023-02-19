a36c1fb823f1e86abe62cabbb8e0ea537f3eb870
danrayfr <danrayrollan98@gmail.com>
2023-02-20 00:52:14 +0800

    import devise gem and style forms

--- Gemfile
+++ Gemfile
@@ -68,3 +68,4 @@ group :test do
 end
 
 gem "tailwindcss-rails", "~> 2.0"
+gem "devise"
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  private 
+
+  # Confirms an admin user
+  def admin_user
+    unless current_user.admin?
+      redirect_to root_url
+      flash[:danger] = "you're not authorized."
+    end
+  end
 end
